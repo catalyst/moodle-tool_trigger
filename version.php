cad7b9646c7b83a85792d0559d48d33cebd9d060
Matt Porritt <mattp@catalyst-au.net>
2018-02-17 16:49:49 +1100

    add intial db schema

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_trigger';
-$plugin->release = '2018021000';
-$plugin->version = 2018021000;
+$plugin->release = '2018021700';
+$plugin->version = 2018021700;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_ALPHA;
