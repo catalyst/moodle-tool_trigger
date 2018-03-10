43f64bdc8993e9c8966a37a23792e84be59734e9
Matt Porritt <mattp@catalyst-au.net>
2018-03-10 17:01:54 +1100

    Initial boilerplate classess for steps

---
+++ classes/steps/filters/base_filter_form.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Base filter step form class.
+ *
+ * @package    tool_trigger
+ * @copyright  Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_trigger\steps;
+
+use tool_trigger\steps\base\base_form;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Base filter step form class.
+ *
+ * @package    tool_trigger
+ * @copyright  Matt Porritt <mattp@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class base_filter_form extends base_form {
+
+    /**
+     * Build form.
+     */
+    public function definition() {
+
+        $mform = $this->_form;
+    }
+
+}
