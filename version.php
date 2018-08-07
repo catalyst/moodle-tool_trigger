33ed82d7671b5d543b70855852c64f6d45a61aa3
Matt Porritt <mattp@catalyst-au.net>
2018-08-07 17:50:06 +1000

    add button to UI to import workflows

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_trigger';
-$plugin->release = '2018080500';
-$plugin->version = 2018080500;
+$plugin->release = '2018080700';
+$plugin->version = 2018080700;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_BETA;
 $plugin->dependencies = array('tool_monitor' => 2015051101);
