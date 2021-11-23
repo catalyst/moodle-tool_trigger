catalyst/moodle-tool_trigger

commit 01950e69828864dad74891b8418a584a11f632ea

Implemented historical data and workflow reruns

db/upgrade.php
@@ -204,5 +204,75 @@ function xmldb_tool_trigger_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2019102200, 'tool', 'trigger');
     }
 
+    if ($oldversion < 2020050100) {
+
+        // Define table tool_trigger_workflow_hist to be created.
+        $table = new xmldb_table('tool_trigger_workflow_hist');
+
+        // Adding fields to table tool_trigger_workflow_hist.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('workflowid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('number', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '15', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('eventid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('event', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('failedstep', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+        // Adding keys to table tool_trigger_workflow_hist.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('eventid', XMLDB_KEY_FOREIGN, array('eventid'), 'tool_trigger_events', array('id'));
+
+        // Conditionally launch create table for tool_trigger_workflow_hist.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table tool_trigger_run_hist to be created.
+        $table = new xmldb_table('tool_trigger_run_hist');
+
+        // Adding fields to table tool_trigger_run_hist.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('workflowid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('runid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('description', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('type', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('stepclass', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('data', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('steporder', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('executed', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('prevstepid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('number', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('results', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('stepconfigid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table tool_trigger_run_hist.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('workflowid', XMLDB_KEY_FOREIGN, array('workflowid'), 'tool_trigger_workflows', array('id'));
+        $table->add_key('run', XMLDB_KEY_FOREIGN, array('runid'), 'tool_trigger_workflow_hist', array('id'));
+
+        // Adding indexes to table tool_trigger_run_hist.
+        $table->add_index('class', XMLDB_INDEX_NOTUNIQUE, array('stepclass'));
+
+        // Conditionally launch create table for tool_trigger_run_hist.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define field debug to be added to tool_trigger_workflows.
+        $table = new xmldb_table('tool_trigger_workflows');
+        $field = new xmldb_field('debug', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0', 'draft');
+
+        // Conditionally launch add field debug.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Trigger savepoint reached.
+        upgrade_plugin_savepoint(true, 2020050100, 'tool', 'trigger');
+    }
+
     return true;
 }

version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_trigger';
-$plugin->release  = 2019112700;
-$plugin->version  = 2019112700;
+$plugin->release  = 2020050100;
+$plugin->version  = 2020050100;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_BETA;
 $plugin->dependencies = array('tool_monitor' => 2015051101);

.travis.yml
@@ -45,6 +45,10 @@ matrix:
       env: DB=pgsql MOODLE_BRANCH=master
     - php: 7.0
       env: DB=mysqli MOODLE_BRANCH=master
+    - php: 7.1
+      env: DB=pgsql MOODLE_BRANCH=master
+    - php: 7.1
+      env: DB=mysqli MOODLE_BRANCH=master
     - php: 7.0
       env: DB=pgsql MOODLE_BRANCH=MOODLE_38_STABLE
     - php: 7.0
@@ -86,6 +90,7 @@ script:
   - moodle-plugin-ci validate
   - moodle-plugin-ci savepoints
   - moodle-plugin-ci mustache
-  - moodle-plugin-ci grunt
+# Grunt is currently broken due to a new way of building JS on 3.8+
+#  - moodle-plugin-ci grunt
   - moodle-plugin-ci phpunit
   - moodle-plugin-ci behat

classes/edit_form.php
@@ -109,6 +109,15 @@ class edit_form extends \moodleform {
         $mform->addHelpButton('workflowrealtime', 'workflowrealtime', 'tool_trigger');
         $mform->setDefault('workflowrealtime', 0);
 
+        // Debug mode data collection.
+        $mform->addElement('advcheckbox',
+            'workflowdebug',
+            get_string('workflowdebug', 'tool_trigger'),
+            'Enable', array(), array(0, 1));
+        $mform->setType('workflowdebug', PARAM_INT);
+        $mform->addHelpButton('workflowdebug', 'workflowdebug', 'tool_trigger');
+        $mform->setDefault('workflowdebug', 0);
+
         // Hidden text field for step JSON.
         $mform->addElement('hidden', 'stepjson');
         $mform->setType('stepjson', PARAM_RAW_TRIMMED);

classes/index_form.php
@@ -1,86 +1,0 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Main Admin settings form class.
- *
- * @package     tool_trigger
- * @copyright   Matt Porritt <mattp@catalyst-au.net>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-namespace tool_trigger;
-
-use html_writer;
-
-defined('MOODLE_INTERNAL') || die();
-
-require_once("$CFG->libdir/formslib.php");
-
-/**
- * Main Admin settings form class.
- *
- * @package     tool_trigger
- * @copyright   Matt Porritt <mattp@catalyst-au.net>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class index_form extends \moodleform {
-
-    /**
-     * Build form for the general setting admin page for plugin.
-     */
-    public function definition() {
-        $config = get_config('tool_trigger');
-        $mform = $this->_form;
-
-        // Cleanup Heading.
-        $mform->addElement('header', 'cleanupsettings', get_string('cleanupsettings', 'tool_trigger'));
-        $desccontent = html_writer::div(get_string('cleanupsettingsdesc', 'tool_trigger'), 'form_description');
-        $mform->addElement('html', $desccontent);
-
-        $cleanupoptions = array(
-            21600 => get_string('numhours', '', 6),
-            43200 => get_string('numhours', '', 12),
-            86400 => get_string('numhours', '', 24),
-            172800 => get_string('numdays', '', 2),
-            604800 => get_string('numdays', '', 7)
-        );
-        $mform->addElement('select', 'timetocleanup', get_string('timetocleanup', 'tool_trigger'), $cleanupoptions);
-        $mform->addHelpButton('timetocleanup', 'timetocleanup', 'tool_trigger');
-        if (isset($config->timetocleanup)) {
-            $mform->setDefault('timetocleanup', $config->timetocleanup);
-        } else {
-            $mform->setDefault('timetocleanup', 86400);
-        }
-
-        // Learning mode settings.
-        $mform->addElement('header', 'learningsettings', get_string('learningsettings', 'tool_trigger'));
-        $desccontent = html_writer::div(get_string('learningsettingsdesc', 'tool_trigger'), 'form_description');
-        $mform->addElement('html', $desccontent);
-
-        $mform->addElement('advcheckbox', 'learning',  get_string ('learning', 'tool_trigger'), 'Enable', array(), array(0, 1));
-        $mform->setType('learning', PARAM_INT);
-        $mform->addHelpButton('learning', 'learning', 'tool_trigger');
-        if (isset($config->learning)) {
-            $mform->setDefault('learning', $config->learning);
-        } else {
-            $mform->setDefault('learning', 0);
-        }
-
-        $this->add_action_buttons();
-    }
-
-}

classes/output/manageworkflows/renderable.php
@@ -67,6 +67,7 @@ class renderable extends \table_sql implements \renderable {
                 'realtime',
                 'numsteps',
                 'lasttriggered',
+                'triggerhistory',
                 'manage'
         ));
         $this->define_headers(array(
@@ -77,6 +78,7 @@ class renderable extends \table_sql implements \renderable {
                 get_string('realtime', 'tool_trigger'),
                 get_string('numsteps', 'tool_trigger'),
                 get_string('lasttriggered', 'tool_trigger'),
+                get_string('triggerhistory', 'tool_trigger'),
                 get_string('manage', 'tool_trigger'),
             )
         );
@@ -138,6 +140,11 @@ class renderable extends \table_sql implements \renderable {
         } else {
             return $workflow->event;
         }
+    }
+
+    public function col_triggerhistory(\tool_trigger\workflow $workflow) {
+        $url = new \moodle_url('/admin/tool/trigger/history.php', array('workflow' => $workflow->id));
+        return \html_writer::link($url, get_string('workflowviewhistory', 'tool_trigger'));
     }
 
     /**

classes/output/workflowhistory/renderer.php
@@ -1,0 +1,178 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Renderer class for workflow history page.
+ *
+ * @package    tool_trigger
+ * @copyright  Peter Burnett <peterburnett@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_trigger\output\workflowhistory;
+defined('MOODLE_INTERNAL') || die;
+require_once(__DIR__.'/run.php');
+require_once(__DIR__.'/workflow.php');
+
+/**
+ * Renderer class for workflow history page.
+ *
+ * @package    tool_trigger
+ * @copyright  Peter Burnett <peterburnett@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class renderer extends \plugin_renderer_base {
+    /**
+     * Sets the SQL for the table, and renders it.
+     *
+     * @param \tool_trigger\output\workflowhistory\renderable $renderable the table to render
+     * @return void
+     */
+    public function render_table($workflowid, $run = null) {
+        $url = new \moodle_url('/admin/tool/trigger/history.php', ['workflow' => $workflowid, 'run' => $run]);
+
+        // Decide which table to render if run is given.
+        if (!empty($run)) {
+            $renderable = new \tool_trigger\output\workflowhistory\runhistory_renderable('runhistory', $url);
+            $sql = (object) [
+                'fields' => '*',
+                'from' => '{tool_trigger_run_hist}',
+                'where' => 'workflowid = :workflow AND runid = :run',
+                'params' => ['workflow' => $workflowid, 'run' => $run]
+            ];
+        } else {
+            $renderable = new \tool_trigger\output\workflowhistory\workflowhistory_renderable('triggerhistory', $url);
+            $sql = (object) [
+                'fields' => '*',
+                'from' => '{tool_trigger_workflow_hist}',
+                'where' => 'workflowid = :workflow',
+                'params' => ['workflow' => $workflowid]
+            ];
+        }
+
+        // Then output the table.
+        $renderable->sql = $sql;
+        $renderable->out($renderable->pagesize, true);
+    }
+
+    public function step_actions_button($step) {
+        $workflow = required_param('workflow', PARAM_INT);
+        $run = required_param('run', PARAM_INT);
+
+        $btn = '';
+
+        $stepdetailsurl = new \moodle_url('/admin/tool/trigger/stepdetails.php', ['id' => $step->id]);
+        $stepdetailslbtn = \html_writer::link($stepdetailsurl,
+            get_string('viewstepinfo', 'tool_trigger'), ['class' => 'btn btn-primary']);
+
+        // Historic urls.
+        $rerunhisturl = new \moodle_url('/admin/tool/trigger/history.php');
+        $rerunhisturl->params(['action' => 'rerunstephist', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $rerunandnexthisturl = new \moodle_url('/admin/tool/trigger/history.php');
+        $rerunandnexthisturl->params(['action' => 'rerunnexthist', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $rerunandfinishhisturl = new \moodle_url('/admin/tool/trigger/history.php');
+        $rerunandfinishhisturl->params(['action' => 'rerunfinishhist', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $executenexthisturl = new \moodle_url('/admin/tool/trigger/history.php');
+        $executenexthisturl->params(['action' => 'executenexthist', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        // Current urls.
+        $reruncurrurl = new \moodle_url('/admin/tool/trigger/history.php');
+        $reruncurrurl->params(['action' => 'rerunstepcurr', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $rerunandnextcurrurl = new \moodle_url('/admin/tool/trigger/history.php');
+        $rerunandnextcurrurl->params(['action' => 'rerunnextcurr', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $rerunandfinishcurrurl = new \moodle_url('/admin/tool/trigger/history.php');
+        $rerunandfinishcurrurl->params(['action' => 'rerunfinishcurr', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $executenextcurrurl = new \moodle_url('/admin/tool/trigger/history.php');
+        $executenextcurrurl->params(['action' => 'executenextcurr', 'id' => $step->id,
+            'sesskey' => sesskey(), 'workflow' => $workflow, 'run' => $run]);
+
+        $btn .= \html_writer::start_div('btn-group');
+        $btn .= $stepdetailslbtn;
+        $btn .= \html_writer::start_tag('button', ['class' => 'btn btn-primary dropdown-toggle dropdown-toggle-split',
+            'data-toggle' => 'dropdown', 'aria-haspopup' => 'true', 'aria-expanded' => 'false']);
+        $btn .= \html_writer::span('toggle-dropdown', 'sr-only');
+        $btn .= \html_writer::end_tag('button');
+        $btn .= \html_writer::start_div('dropdown-menu dropdown-menu-right');
+        $btn .= \html_writer::tag('h6', get_string('actionscurrent', 'tool_trigger'),
+            ['class' => 'dropdown-header']);
+        $btn .= \html_writer::link($reruncurrurl, get_string('rerunstep', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($rerunandnextcurrurl, get_string('rerunstepandnext', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($rerunandfinishcurrurl, get_string('rerunstepandfinish', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($executenextcurrurl, get_string('executenext', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::div('', 'dropdown-divider');
+        $btn .= \html_writer::tag('h6', get_string('actionshistoric', 'tool_trigger'),
+            ['class' => 'dropdown-header']);
+        $btn .= \html_writer::link($rerunhisturl, get_string('rerunstep', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($rerunandnexthisturl, get_string('rerunstepandnext', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($rerunandfinishhisturl, get_string('rerunstepandfinish', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::link($executenexthisturl, get_string('executenext', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::end_div() . \html_writer::end_div();
+
+        return $btn;
+    }
+
+    public function run_actions_button($run) {
+        $btn = '';
+        $viewurl = new \moodle_url('/admin/tool/trigger/history.php', array('run' => $run->id, 'workflow' => $run->workflowid));
+        $viewbtn = \html_writer::link($viewurl, get_string('viewdetailedrun', 'tool_trigger'), ['class' => 'btn btn-primary']);
+
+        $reruncurrurl = new \moodle_url('/admin/tool/trigger/history.php',
+            ['action' => 'rerunworkflowcurr', 'id' => $run->id, 'workflow' => $run->workflowid, 'sesskey' => sesskey()]);
+        $rerunhisturl = new \moodle_url('/admin/tool/trigger/history.php',
+            ['action' => 'rerunworkflowhist', 'id' => $run->id, 'workflow' => $run->workflowid, 'sesskey' => sesskey()]);
+
+        $btn .= \html_writer::start_div('btn-group');
+        $btn .= $viewbtn;
+        $btn .= \html_writer::start_tag('button', ['class' => 'btn btn-primary dropdown-toggle dropdown-toggle-split',
+            'data-toggle' => 'dropdown', 'aria-haspopup' => 'true', 'aria-expanded' => 'false']);
+        $btn .= \html_writer::span('toggle-dropdown', 'sr-only');
+        $btn .= \html_writer::end_tag('button');
+        $btn .= \html_writer::start_div('dropdown-menu dropdown-menu-right');
+        $btn .= \html_writer::tag('h6', get_string('actionscurrent', 'tool_trigger'),
+            ['class' => 'dropdown-header']);
+        $btn .= \html_writer::link($reruncurrurl, get_string('rerunworkflow', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::div('', 'dropdown-divider');
+        $btn .= \html_writer::tag('h6', get_string('actionshistoric', 'tool_trigger'),
+            ['class' => 'dropdown-header']);
+        $btn .= \html_writer::link($rerunhisturl, get_string('rerunworkflow', 'tool_trigger'),
+            ['class' => 'dropdown-item']);
+        $btn .= \html_writer::end_div() . \html_writer::end_div();
+
+        return $btn;
+    }
+}

commit c1f3f5a2049a03a40c5fed7b7b5e743716444b76

Add missing foreign key for 'stepconfigid' upgrades
Issue when upgrading from a lower version - started from 01950e69828864dad74891b8418a584a11f632ea

db/upgrade.php
@@ -337,5 +337,20 @@ function xmldb_tool_trigger_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2021030403, 'tool', 'trigger');
     }
 
+    if ($oldversion < 2021030404) {
+
+        // Define key stepconfigid (foreign) to be added to tool_trigger_run_hist.
+        $table = new xmldb_table('tool_trigger_run_hist');
+        $key = new xmldb_key('stepconfigid', XMLDB_KEY_FOREIGN, ['stepconfigid'], 'tool_trigger_steps', ['id']);
+
+        // Launch add key stepconfigid.
+        if (!$table->getKey($key->getName())) {
+            $dbman->add_key($table, $key);
+        }
+
+        // Trigger savepoint reached.
+        upgrade_plugin_savepoint(true, 2021030404, 'tool', 'trigger');
+    }
+
     return true;
 }

version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_trigger';
-$plugin->release  = 2021030403;
-$plugin->version  = 2021030403;
+$plugin->release  = 2021030404;
+$plugin->version  = 2021030404;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('tool_monitor' => 2015051101);