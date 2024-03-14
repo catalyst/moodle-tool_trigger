e1a767b9f248d1b3f7bb6f98626bce25aca2d9a1
Benjamin Walker <benjaminwalker@catalyst-au.net>
2024-03-14 14:30:07 +1000

    Add secrets to workflow

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_trigger';
-$plugin->version   = 2024031400;
-$plugin->release   = 2024031400; // Match release exactly to version.
+$plugin->version   = 2024031401;
+$plugin->release   = 2024031401; // Match release exactly to version.
 $plugin->requires  = 2021051701;
 $plugin->supported = [311, 401];
 $plugin->maturity  = MATURITY_STABLE;
